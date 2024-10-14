f315f4ea2666b042c10dd1aca85b0f02022821e5
Darshil Patel <93610569+pdarsh58@users.noreply.github.com>
2024-10-14 17:10:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import pickle
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
+export NVIDIA_API_KEY = "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"
+
 st.set_page_config(layout="wide")
 
 # Component #1 - Document Upload
